3ca484834f980f796a1ce354e1d4d7cb3a63c7e3
KimJuHyeong <kimvs2001@naver.com>
2017-01-01 22:24:12 +0900

    start

---
+++ src/game_/Window.java
@@ -0,0 +1,12 @@
+package game_;
+
+import java.awt.GridLayout;
+import java.awt.event.KeyListener;
+import java.util.ArrayList;
+import javax.swing.JFrame;
+
+class Window extends JFrame{
+	private static final long serialCersionUID = -2542001418764869760L;
+	public static ArrayList<ArrayList<DataOfSquare>> Grid;
+	
+}
